8191d06e50e9fe44b05b440e326084c728485edc
Maxime Griot <yamashi@live.com>
2020-10-15 16:53:01 +0200

    Same thing but fallout 4

---
+++ Code/client/src/Games/Fallout4/Forms/TESNPC.cpp
@@ -0,0 +1,26 @@
+
+#if TP_FALLOUT4
+
+#include <Games/Fallout4/Forms/TESNPC.h>
+
+TP_THIS_FUNCTION(TSetLeveledNpc, TESNPC*, TESNPC, TESNPC*);
+static TSetLeveledNpc* RealSetLeveledNpc = nullptr;
+
+TESNPC* TP_MAKE_THISCALL(HookSetLeveledNpc, TESNPC, TESNPC* apSelectedNpc)
+{
+    spdlog::info("For TESNPC: {}, spawning: {}", apThis->fullName.value, apSelectedNpc->fullName.value);
+
+    return ThisCall(RealSetLeveledNpc, apThis, RTTI_CAST(TESForm::GetById(0x3B547), TESForm, TESNPC));
+}
+
+
+static TiltedPhoques::Initializer s_npcInitHooks([]() {
+    POINTER_FALLOUT4(TSetLeveledNpc, s_SetLeveledNpc, 0x140149340 - 0x140000000);
+
+    RealSetLeveledNpc = s_SetLeveledNpc.Get();
+
+    //TP_HOOK(&RealSetLeveledNpc, HookSetLeveledNpc);
+});
+
+
+#endif
