81fbf7c6241cfbf213d4414a40b75687cb94656b
Maxime Griot <yamashi@live.com>
2020-10-15 16:47:24 +0200

    Hooked the function that sets the NPC for leveled characters

---
+++ Code/client/src/Games/Skyrim/Forms/TESNPC.cpp
@@ -0,0 +1,26 @@
+
+#if TP_SKYRIM
+
+#include <Games/Skyrim/Forms/TESNPC.h>
+
+TP_THIS_FUNCTION(TSetLeveledNpc, TESNPC*, TESNPC, TESNPC*);
+static TSetLeveledNpc* RealSetLeveledNpc = nullptr;
+
+TESNPC* TP_MAKE_THISCALL(HookSetLeveledNpc, TESNPC, TESNPC* apSelectedNpc)
+{
+    spdlog::info("For TESNPC: {}, spawning: {}", apThis->fullName.value, apSelectedNpc->fullName.value);
+
+    return ThisCall(RealSetLeveledNpc, apThis, RTTI_CAST(TESForm::GetById(0x3B547), TESForm, TESNPC));
+}
+
+
+static TiltedPhoques::Initializer s_npcInitHooks([]() {
+    POINTER_SKYRIMSE(TSetLeveledNpc, s_SetLeveledNpc, 0x14018C390 - 0x140000000);
+
+    RealSetLeveledNpc = s_SetLeveledNpc.Get();
+
+    //TP_HOOK(&RealSetLeveledNpc, HookSetLeveledNpc);
+});
+
+
+#endif
