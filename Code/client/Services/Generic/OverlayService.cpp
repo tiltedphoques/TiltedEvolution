36ca29667ab971032a5c1fa41b005111b8bcc496
Dragonisser <dragonisser@googlemail.com>
2021-03-15 09:58:55 +0000

    Remove debugbreak

--- Code/client/Services/Generic/OverlayService.cpp
+++ Code/client/Services/Generic/OverlayService.cpp
@@ -54,6 +54,13 @@ OverlayService::~OverlayService() noexcept
 
 void OverlayService::Create(RenderSystemD3D11* apRenderSystem) noexcept
 {
+    m_pProvider = TiltedPhoques::MakeUnique<D3D11RenderProvider>(apRenderSystem);
+    m_pOverlay = new OverlayApp(m_pProvider.get(), new ::OverlayClient(m_transport, m_pProvider->Create()));
+
+    if (!m_pOverlay->Initialize())
+        spdlog::debug("Overlay could not be initialised");
+
+    m_pOverlay->GetClient()->Create();
 }
 
 void OverlayService::Render() const noexcept
