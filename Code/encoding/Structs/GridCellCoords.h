f41dbc616cc31e35ffef09a1f4678baa72f03787
Robbe Bryssinck <robbebryssinck@gmail.com>
2021-05-12 11:09:46 +0200

    Fixed extra qualification compilation error

--- Code/encoding/Structs/GridCellCoords.h
+++ Code/encoding/Structs/GridCellCoords.h
@@ -8,9 +8,9 @@ struct GridCellCoords
 {
     static const int32_t m_gridsToLoad = 5;
     static GridCellCoords CalculateGridCellCoords(const float aX, const float aY) noexcept;
-    static bool GridCellCoords::AreGridCellsOverlapping(const GridCellCoords* aCoords1,
+    static bool AreGridCellsOverlapping(const GridCellCoords* aCoords1,
                                                         const GridCellCoords* aCoords2) noexcept;
-    static bool GridCellCoords::IsCellInGridCell(const GridCellCoords* aCell, const GridCellCoords* aGridCell) noexcept;
+    static bool IsCellInGridCell(const GridCellCoords* aCell, const GridCellCoords* aGridCell) noexcept;
 
     GridCellCoords() = default;
     GridCellCoords(int32_t aX, int32_t aY) noexcept;
