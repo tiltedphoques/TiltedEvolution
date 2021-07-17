6bfa413c4a426cd37a3b0891f12f416d00c45108
Robbe Bryssinck <robbebryssinck@gmail.com>
2021-07-17 22:51:29 +0200

    Revert "Skyrim dwarven sphere animation sync"
    This reverts commit 3ccc1e631676e0071246f1aa77bcc9e030f92569.

--- Code/encoding/Structs/Skyrim/AnimationGraphDescriptorManager.cpp
+++ Code/encoding/Structs/Skyrim/AnimationGraphDescriptorManager.cpp
@@ -14,7 +14,6 @@
 #include <Structs/Skyrim/AnimationGraphDescriptor_HorseRootBehavior.h>
 #include <Structs/Skyrim/AnimationGraphDescriptor_AtronachFrostRootBehavior.h>
 #include <Structs/Skyrim/AnimationGraphDescriptor_AtronachStormBehavior.h>
-#include <Structs/Skyrim/AnimationGraphDescriptor_SphereRoot_Behavior.h>
 
 AnimationGraphDescriptorManager::AnimationGraphDescriptorManager() noexcept
 {
@@ -33,6 +32,5 @@ AnimationGraphDescriptorManager::AnimationGraphDescriptorManager() noexcept
         AnimationGraphDescriptor_HorseRootBehavior initAnimationGraphDescriptor_HorseRootBehavior(*this);
         AnimationGraphDescriptor_AtronachFrostRootBehavior initAnimationGraphDescriptor_AtronachFrostRootBehavior(*this);
         AnimationGraphDescriptor_AtronachStormBehavior initAnimationGraphDescriptor_AtronachStormBehavior(*this);
-        AnimationGraphDescriptor_SphereRoot_Behavior initAnimationGraphDescriptor_SphereRoot_Behavior(*this);
     });
 }
--- Code/encoding/Structs/Skyrim/AnimationGraphDescriptor_SphereRoot_Behavior.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct AnimationGraphDescriptorManager;
-
-struct AnimationGraphDescriptor_SphereRoot_Behavior
-{
-    AnimationGraphDescriptor_SphereRoot_Behavior(AnimationGraphDescriptorManager& aManager);
-};
