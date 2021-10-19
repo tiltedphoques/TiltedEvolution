f865746adfeb06cf37d6f4b1f0b338569410c317
Robbe Bryssinck <robbebryssinck@gmail.com>
2021-10-19 20:56:22 +0200

    Added assert macro

--- Code/client/Games/Skyrim/EquipManager.cpp
+++ Code/client/Games/Skyrim/EquipManager.cpp
@@ -117,13 +117,6 @@ void* EquipManager::Equip(Actor* apActor, TESForm* apItem, BSExtraDataList* apEx
 
     const auto result = ThisCall(s_equipFunc, this, apActor, apItem, apExtraDataList, aCount, aSlot, aUnk1, aPreventEquip, aUnk2, aUnk3);
 
-    // TODO: hacky as fuck, pls fix
-    auto* pAmmo = RTTI_CAST(apItem, TESForm, TESAmmo);
-    if (pAmmo)
-    {
-        apActor->processManager->middleProcess->pAmmo = pAmmo;
-    }
-
     return result;
 }
 
