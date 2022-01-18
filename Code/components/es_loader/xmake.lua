401208aa7194cacca486f49b5db935b44be1fe9e
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-01-18 09:49:06 +0100

    tweak: moved ESLoader to Components

---
+++ Code/components/es_loader/xmake.lua
@@ -0,0 +1,45 @@
+
+add_requires("zlib")
+
+target("ESLoader")
+	set_kind("static")
+	set_group("Components")
+    add_configfiles("BuildInfo.h.in")
+    set_pcxxheader("stdafx.h")
+	add_includedirs(
+		".",
+		"../",
+		"../../", 
+		"../../../build", 
+		{public = true})
+	add_headerfiles("stdafx.h", "**.h", {prefixdir = "ESLoader"})
+	add_files("**.cpp", "stdafx.cpp")
+	add_packages(
+		"tiltedcore",
+        "spdlog",
+        "hopscotch-map",
+        "zlib")
+
+
+target("ESLoader-Test")
+	set_kind("binary")
+	set_group("Components")
+	add_configfiles("BuildInfo.h.in")
+    set_pcxxheader("stdafx.h")
+	add_includedirs(
+		".",
+		"../",
+		"../../", 
+		"../../../build", 
+		{public = true})
+	add_headerfiles(
+		"**.h")
+	add_files(
+		"**.cpp",
+		"../../../build/TestMain.cpp")
+	add_packages(
+		"tiltedcore", 
+		"hopscotch-map", 
+		"gtest",
+		"spdlog",
+		"zlib")
