8fdfc47c6c511ed2679f2bce106c92161e89f30b
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-03-15 02:59:52 +0100

    tweak: cache last used combat skill

--- Code/client/Games/Skyrim/PlayerCharacter.cpp
+++ Code/client/Games/Skyrim/PlayerCharacter.cpp
@@ -6,6 +6,9 @@
 
 #include <World.h>
 
+#include <Games/Skyrim/Forms/ActorValueInfo.h>
+#include <Games/ActorExtension.h>
+
 TP_THIS_FUNCTION(TPickUpItem, char, PlayerCharacter, TESObjectREFR* apObject, int32_t aCount, bool aUnk1, bool aUnk2);
 TP_THIS_FUNCTION(TAddSkillExperience, void, PlayerCharacter, int32_t aSkill, float aExperience);
 
@@ -25,6 +28,19 @@ char TP_MAKE_THISCALL(HookPickUpItem, PlayerCharacter, TESObjectREFR* apObject, 
 
 void TP_MAKE_THISCALL(HookAddSkillExperience, PlayerCharacter, int32_t aSkill, float aExperience)
 {
+    // TODO: armor skills?
+    static const Set<int32_t> combatSkills{ActorValueInfo::kAlteration, ActorValueInfo::kConjuration, ActorValueInfo::kDestruction,
+                                 ActorValueInfo::kIllusion, ActorValueInfo::kRestoration, ActorValueInfo::kOneHanded,
+                                 ActorValueInfo::kTwoHanded, ActorValueInfo::kMarksman, ActorValueInfo::kBlock};
+
+    if (combatSkills.contains(aSkill))
+    {
+        spdlog::info("Set new last used combat skill.");
+        apThis->GetExtension()->LastUsedCombatSkill = aSkill;
+    }
+
+    // TODO: send event
+
     spdlog::info("Skill: {}, experience: {}", aSkill, aExperience);
     ThisCall(RealAddSkillExperience, apThis, aSkill, aExperience);
 }
