cc82b5930f19f8f0c80045ae447b6f1f7d5cac58
Force67 <prelink835@gmail.com>
2022-03-21 17:06:47 +0100

    fix(settings): cleanup

--- Code/client/Games/Skyrim/BSMain/Setting.cpp
+++ Code/client/Games/Skyrim/BSMain/Setting.cpp
@@ -13,30 +13,22 @@ Setting::SETTING_TYPE Setting::DataType(const char* apKey)
     case 'S':
     case 's':
         return ST_STRING;
-        break;
     case 'a':
         return ST_RGBA;
     case 'b':
         return ST_BINARY;
-        break;
     case 'c':
         return ST_CHAR;
-        break;
     case 'f':
         return ST_FLOAT;
-        break;
     case 'h':
         return ST_UCHAR;
-        break;
     case 'i':
         return ST_INT;
-        break;
     case 'r':
         return ST_RGB;
-        break;
     case 'u':
         return ST_UINT;
-        break;
     default:
         return ST_NONE;
     }
