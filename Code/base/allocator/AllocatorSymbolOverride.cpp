fd5992c70f5e1a5e3afff564a114bf64f0b0e85d
Force67 <prelink835@gmail.com>
2022-04-16 19:51:49 +0200

    tweak: Use the right allocator, lol

--- Code/base/allocator/AllocatorSymbolOverride.cpp
+++ Code/base/allocator/AllocatorSymbolOverride.cpp
@@ -2,62 +2,63 @@
 
 #include <TiltedCore/Allocator.hpp>
 
-void* TPAlloc(const size_t acSize)
+// not private cause thse can be referenced as external symbols
+void* TiltedAlloc(const size_t acSize)
 {
-    return TiltedPhoques::Allocator::Get()->Allocate(acSize);
+    return TiltedPhoques::Allocator::GetDefault()->Allocate(acSize);
 }
 
-void TPFree(void* apBlock)
+void TiltedFree(void* apBlock)
 {
-    TiltedPhoques::Allocator::Get()->Free(apBlock);
+    TiltedPhoques::Allocator::GetDefault()->Free(apBlock);
 }
 
 void* operator new(size_t size)
 {
-    return TPAlloc(size);
+    return TiltedAlloc(size);
 }
 
 void operator delete(void* p) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
 
 void* operator new[](size_t size)
 {
-    return TPAlloc(size);
+    return TiltedAlloc(size);
 }
 
 void operator delete[](void* p) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
 
 void* operator new(size_t size, const std::nothrow_t&) noexcept
 {
-    return TPAlloc(size);
+    return TiltedAlloc(size);
 }
 
 void* operator new[](size_t size, const std::nothrow_t&) noexcept
 {
-    return TPAlloc(size);
+    return TiltedAlloc(size);
 }
 
 void operator delete(void* p, const std::nothrow_t&) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
 
 void operator delete[](void* p, const std::nothrow_t&) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
 
 void operator delete(void* p, size_t) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
 
 void operator delete[](void* p, size_t) noexcept
 {
-    TPFree(p);
+    TiltedFree(p);
 }
