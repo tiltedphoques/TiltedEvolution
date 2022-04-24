f1f128c0e5a5c0a1dff7c602d41609692cfd7180
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-04-24 17:03:04 +0200

    docs: systems

--- Code/client/Systems/InterpolationSystem.h
+++ Code/client/Systems/InterpolationSystem.h
@@ -3,6 +3,9 @@
 struct World;
 struct Actor;
 
+/**
+* @brief Manages interpolation of movement and animations.
+*/
 struct InterpolationSystem
 {
     static void Update(Actor* apActor, InterpolationComponent& aInterpolationComponent, uint64_t aTick) noexcept;
