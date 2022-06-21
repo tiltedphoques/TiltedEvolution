b51fb5af9aee4e5c9209d11a0abea7e6286db828
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-06-21 22:52:15 +0200

    tweak: disable invite button if not leader

--- Code/skyrim_ui/src/app/components/player-list/player-list.component.ts
+++ Code/skyrim_ui/src/app/components/player-list/player-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnDestroy, OnInit, Output, ViewEncapsulation }
 import { Player } from "src/app/models/player";
 import { PlayerList } from "src/app/models/player-list";
 import { ClientService } from "src/app/services/client.service";
+import { GroupService } from "src/app/services/group.service";
 import { PlayerListService } from "src/app/services/player-list.service";
 
 @Component({
@@ -13,7 +14,8 @@ import { PlayerListService } from "src/app/services/player-list.service";
 export class PlayerListComponent implements OnInit, OnDestroy {
 
   constructor(public playerListService: PlayerListService,
-              private clientService: ClientService
+              private clientService: ClientService,
+              private groupService: GroupService
   ) { }
 
   ngOnInit(): void {
@@ -32,6 +34,10 @@ export class PlayerListComponent implements OnInit, OnDestroy {
 
   public get isConnected(): boolean {
     return this.clientService.connectionStateChange.value;
+  }
+
+  public get isPartyLeader(): boolean {
+    return this.groupService.group.value.isEnabled && this.groupService.group.value.owner == this.clientService.localPlayerId;
   }
 
   public teleportToPlayer(playerId: number) {
