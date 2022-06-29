46e2586dfded9c11c04bff8f0ffd509c714a6a6b
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-06-29 16:35:04 +0200

    tweak(ft): address lib error

--- Code/client/main.cpp
+++ Code/client/main.cpp
@@ -11,19 +11,31 @@ std::unique_ptr<TiltedOnlineApp> g_appInstance{nullptr};
 
 extern HICON g_SharedWindowIcon;
 
-// TODO: ft
 static void ShowAddressLibraryError(const wchar_t* apGamePath)
 {
+#if TP_SKYRIM64
     auto errorDetail = fmt::format(L"Looking for it here: {}\\Data\\SKSE\\Plugins", apGamePath);
 
     Base::TaskDialog dia(g_SharedWindowIcon, L"Error", L"Failed to load Skyrim Address Library",
                          L"Make sure to use the All in one Anniversary Edition", errorDetail.c_str());
+#elif TP_FALLOUT4
+    auto errorDetail = fmt::format(L"Looking for it here: {}\\Data\\F4SE\\Plugins", apGamePath);
+
+    Base::TaskDialog dia(g_SharedWindowIcon, L"Error", L"Failed to load Fallout 4 Address Library",
+                         L"", errorDetail.c_str());
+#endif
+
     dia.AppendButton(0xBEEF, L"Visit Address Library modpage on nexusmods.com");
     const int result = dia.Show();
     if (result == 0xBEEF)
     {
+#if TP_SKYRIM64
         ShellExecuteW(nullptr, L"open", LR"(https://www.nexusmods.com/skyrimspecialedition/mods/32444?tab=files)",
                       nullptr, nullptr, SW_SHOWNORMAL);
+#elif TP_FALLOUT4
+        ShellExecuteW(nullptr, L"open", LR"(https://www.nexusmods.com/fallout4/mods/47327)",
+                      nullptr, nullptr, SW_SHOWNORMAL);
+#endif
     }
 
     exit(4);
