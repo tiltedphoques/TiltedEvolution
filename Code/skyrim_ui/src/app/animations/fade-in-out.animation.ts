9b1b3de1268fdf33fcc58ef259ed8f66fa7c539a
Sebastian Comans <sebastian.comans@i40mc.de>
2022-07-15 17:53:09 +0200

    tweak: pointer-events can't be animated

--- Code/skyrim_ui/src/app/animations/fade-in-out.animation.ts
+++ Code/skyrim_ui/src/app/animations/fade-in-out.animation.ts
@@ -5,13 +5,11 @@ export const fadeInOutAnimation = trigger('fadeInOut', [
   state('void', style({
     'visibility': 'hidden',
     'opacity': '0',
-    'pointer-events': 'none',
   })),
   transition('void => *', [
     animate('300ms ease-in', style({
       'visibility': 'visible',
       'opacity': '1',
-      'pointer-events': 'all',
     })),
   ]),
   transition('* => void', [
