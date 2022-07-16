784d0d53e0eb22ac0358b54231b5806e211aca5a
kulcris <browncjay@gmail.com>
2022-07-16 11:14:29 -0500

    Revert "Fix: Health Bar Overflow"
    This reverts commit 8171e2d89566662f5c4f947ae1f96f4ba09b608a.

--- Code/client/Services/Generic/OverlayService.cpp
+++ Code/client/Services/Generic/OverlayService.cpp
@@ -102,8 +102,6 @@ float CalculateHealthPercentage(Actor* apActor) noexcept
     float percentage = health / maxHealth * 100.f;
     if (percentage < 0.f)
         percentage = 0.f;
-	if (percentage > 100.f)
-		percentage = 100.f;
 
     return percentage;
 }
