0920094027364de3b6141ee07199d2fd1f4c0ff5
Даниил <sidiusz@ya.ru>
2022-07-29 20:56:12 +0400

    Add files via upload

--- Code/skyrim_ui/src/app/transloco-root.module.ts
+++ Code/skyrim_ui/src/app/transloco-root.module.ts
@@ -26,6 +26,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
         availableLangs: [
           { id: 'en', label: 'English' },
           { id: 'de', label: 'Deutsch' },
+		  { id: 'ru', label: 'Русский' },
           { id: 'overwrite', label: 'Custom' },
         ],
         defaultLang: 'en',
