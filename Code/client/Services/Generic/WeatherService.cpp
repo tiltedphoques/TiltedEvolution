c2174acfa12ffb0c0da30948bd4a3d4dadb3c7d7
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-09-06 00:21:16 +0200

    tweak: check weather for changes as leader

--- Code/client/Services/Generic/WeatherService.cpp
+++ Code/client/Services/Generic/WeatherService.cpp
@@ -1,17 +1,25 @@
 #include <Services/WeatherService.h>
 
+#include <Events/UpdateEvent.h>
 #include <Events/DisconnectedEvent.h>
 #include <Events/PartyJoinedEvent.h>
 #include <Events/PartyLeftEvent.h>
 
 #include <Sky/Sky.h>
+#include <Forms/TESWeather.h>
 
 WeatherService::WeatherService(World& aWorld, TransportService& aTransport, entt::dispatcher& aDispatcher)
     : m_world(aWorld), m_transport(aTransport)
 {
+    m_updateConnection = aDispatcher.sink<UpdateEvent>().connect<&WeatherService::OnUpdate>(this);
     m_disconnectConnection = aDispatcher.sink<DisconnectedEvent>().connect<&WeatherService::OnDisconnected>(this);
     m_partyJoinedConnection = aDispatcher.sink<PartyJoinedEvent>().connect<&WeatherService::OnPartyJoinedEvent>(this);
     m_partyLeftConnection = aDispatcher.sink<PartyLeftEvent>().connect<&WeatherService::OnPartyLeftEvent>(this);
+}
+
+void WeatherService::OnUpdate(const UpdateEvent& acEvent) noexcept
+{
+    RunWeatherUpdates(acEvent.Delta);
 }
 
 void WeatherService::OnDisconnected(const DisconnectedEvent& acEvent) noexcept
@@ -27,6 +35,28 @@ void WeatherService::OnPartyJoinedEvent(const PartyJoinedEvent& acEvent) noexcep
 void WeatherService::OnPartyLeftEvent(const PartyLeftEvent& acEvent) noexcept
 {
     RegainControlOfWeather();
+}
+
+void WeatherService::RunWeatherUpdates(const double acDelta) noexcept
+{
+    Sky* pSky = Sky::Get();
+    if (!pSky)
+        return;
+
+    TESWeather* pWeather = pSky->GetWeather();
+    if (!pWeather)
+    {
+        m_cachedWeatherId = 0;
+        return;
+    }
+
+    // Have to manually check each frame because there's no singular SetWeather being used in-game.
+    if (pWeather->formID == m_cachedWeatherId)
+        return;
+
+    m_cachedWeatherId = pWeather->formID;
+
+    // TODO: send out new weather
 }
 
 void WeatherService::RegainControlOfWeather() noexcept
--- Code/client/Services/WeatherService.h
+++ Code/client/Services/WeatherService.h
@@ -2,6 +2,7 @@
 
 struct World;
 struct TransportService;
+struct UpdateEvent;
 struct DisconnectedEvent;
 struct PartyJoinedEvent;
 struct PartyLeftEvent;
@@ -18,9 +19,12 @@ struct WeatherService
     TP_NOCOPYMOVE(WeatherService);
 
 protected:
+    void OnUpdate(const UpdateEvent& acEvent) noexcept;
     void OnDisconnected(const DisconnectedEvent& acEvent) noexcept;
     void OnPartyJoinedEvent(const PartyJoinedEvent& acEvent) noexcept;
     void OnPartyLeftEvent(const PartyLeftEvent& acEvent) noexcept;
+
+    void RunWeatherUpdates(const double acDelta) noexcept;
 
     void RegainControlOfWeather() noexcept;
 
@@ -29,7 +33,9 @@ private:
     TransportService& m_transport;
 
     bool m_isInParty{};
+    uint32_t m_cachedWeatherId{};
 
+    entt::scoped_connection m_updateConnection;
     entt::scoped_connection m_disconnectConnection;
     entt::scoped_connection m_partyJoinedConnection;
     entt::scoped_connection m_partyLeftConnection;
