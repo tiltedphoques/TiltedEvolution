2f0996f61ed2f479e3cffc1351ae14c52c4e46f4
stenliis <fuciks@seznam.cz>
2022-09-11 14:58:21 +0200

    Add Czech language

--- Code/skyrim_ui/src/app/transloco-root.module.ts
+++ Code/skyrim_ui/src/app/transloco-root.module.ts
@@ -30,6 +30,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
           { id: 'fr', label: 'Français' },
           { id: 'zh-CN', label: '中文（中国）'},
           { id: 'nl', label: 'Nederlands' },
+          { id: 'cs', label: 'Čeština'},
           { id: 'overwrite', label: 'Custom' },
         ],
         defaultLang: 'en',
