a931f0ddf5ff1525874e2179034e7c263f88ccde
Shynd <Outshynd@users.noreply.github.com>
2022-09-13 03:19:59 -0400

    change docker hub repository to official

--- docker-multiarch-build.sh
+++ docker-multiarch-build.sh
@@ -5,10 +5,10 @@ docker buildx create --name str-multiarch --use
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 #build the builder for both x86_64 and arm64 platforms
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.builder -t outshynd/multiarch-builder:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.builder -t tiltedphoques/multiarch-builder:latest --push .
 
 #build the server for both x86_64 and arm64 platforms
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t outshynd/str-reborn-server:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t tiltedphoques/str-reborn-server:latest --push .
 
 #remove buildx builder
 docker buildx rm str-multiarch
