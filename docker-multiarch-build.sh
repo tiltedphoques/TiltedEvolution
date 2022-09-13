1e98fb3622a6487be5b8be45beadae43086f8f6c
Shynd <Outshynd@users.noreply.github.com>
2022-09-13 07:50:32 -0400

    fix typo(s)

--- docker-multiarch-build.sh
+++ docker-multiarch-build.sh
@@ -8,7 +8,7 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.builder -t tiltedphoques/multiarch-builder:latest --push .
 
 #build the server for both x86_64 and arm64 platforms
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t tiltedphoques/str-reborn-server:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t tiltedphoques/st-reborn-server:latest --push .
 
 #remove buildx builder
 docker buildx rm str-multiarch
