3dc212e6d020888e249c8b34a0c25c1abb841028
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-09-21 17:35:45 +0200

    feat: crash dump dialogue

--- Code/client/CrashHandler.cpp
+++ Code/client/CrashHandler.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <strsafe.h>
 
+#include <base/dialogues/win/TaskDialog.h>
+
 using time_point = std::chrono::system_clock::time_point;
 
 std::string SerializeTimePoint(const time_point& time, const std::string& format)
@@ -20,27 +22,46 @@ std::string SerializeTimePoint(const time_point& time, const std::string& format
     return ss.str();
 }
 
+extern HICON g_SharedWindowIcon;
+
 LONG WINAPI VectoredExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
 {
     if (pExceptionInfo->ExceptionRecord->ExceptionCode == 0xC0000005)
     {
-        MINIDUMP_EXCEPTION_INFORMATION M;
-        char dumpPath[MAX_PATH];
-
-        M.ThreadId = GetCurrentThreadId();
-        M.ExceptionPointers = pExceptionInfo;
-        M.ClientPointers = 0;
-
         std::ostringstream oss;
         oss << "crash_" << SerializeTimePoint(std::chrono::system_clock::now(), "UTC_%Y-%m-%d_%H-%M-%S") << ".dmp";
+
+        char dumpPath[MAX_PATH];
 
         GetModuleFileNameA(NULL, dumpPath, sizeof(dumpPath));
         std::filesystem::path modulePath(dumpPath);
         auto subPath = modulePath.parent_path();
 
+        subPath /= oss.str();
+
+        // TODO: use this if instead before merging
+//#if (!IS_MASTER)
+#if 1
+        auto dumpLocation = fmt::format(L"The crash dump file will have the following name: {}", subPath.filename().c_str());
+
+        Base::TaskDialog dia(g_SharedWindowIcon, L"Crash", L"A crash has occurred.",
+                             L"Do you want to generate a crash dump?",
+                             dumpLocation.c_str());
+
+        dia.AppendButton(0xBEED, L"Yes");
+        dia.AppendButton(0xBEEF, L"No");
+        const int result = dia.Show();
+
+        if (result != 0xBEED)
+            return EXCEPTION_EXECUTE_HANDLER;
+#endif
+
         CrashHandler::RemovePreviousDump(subPath);
 
-        subPath /= oss.str();
+        MINIDUMP_EXCEPTION_INFORMATION M;
+        M.ThreadId = GetCurrentThreadId();
+        M.ExceptionPointers = pExceptionInfo;
+        M.ClientPointers = 0;
 
         auto hDumpFile =
             CreateFileA(subPath.string().c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
