741ea0d8987bc62f6a56a25bcea87855b014ddb7
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-18 18:44:47 +0200

    fix: fallout together build

--- Code/client/Games/Combat/CombatController.cpp
+++ Code/client/Games/Combat/CombatController.cpp
@@ -1,12 +1,16 @@
 #include "CombatController.h"
 
+// TODO: ft
+
 TP_THIS_FUNCTION(TUpdateTarget, bool, CombatController);
 static TUpdateTarget* RealUpdateTarget = nullptr;
 
 bool TP_MAKE_THISCALL(HookUpdateTarget, CombatController)
 {
+#if TP_SKYRIM64
     if (!apThis->startedCombat)
         return TiltedPhoques::ThisCall(RealUpdateTarget, apThis);
+#endif
 
     return false;
 }
@@ -16,11 +20,12 @@ static TSetTarget* RealSetTarget = nullptr;
 
 void TP_MAKE_THISCALL(HookSetTarget, CombatController, Actor* apTarget)
 {
+#if TP_SKYRIM64
     spdlog::info("In game calls SetTarget, Form id: {:X}", apTarget->formID);
     TiltedPhoques::ThisCall(RealSetTarget, apThis, apTarget);
+#endif
 }
 
-// TODO: ft
 void CombatController::SetTarget(Actor* apTarget)
 {
 #if TP_SKYRIM64
@@ -32,6 +37,7 @@ void CombatController::SetTarget(Actor* apTarget)
 static TiltedPhoques::Initializer s_combatControllerHooks(
     []()
     {
+    #if TP_SKYRIM64
         POINTER_SKYRIMSE(TSetTarget, s_setTarget, 33235);
         POINTER_SKYRIMSE(TUpdateTarget, s_updateTarget, 33236);
 
@@ -40,5 +46,6 @@ static TiltedPhoques::Initializer s_combatControllerHooks(
 
         TP_HOOK(&RealSetTarget, HookSetTarget);
         TP_HOOK(&RealUpdateTarget, HookUpdateTarget);
+    #endif
     });
 
