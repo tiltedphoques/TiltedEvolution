4dceeab974ebbe43cacfb0862d0308ae8ec2af42
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-29 15:45:31 +0200

    fix: compile error

--- Code/client/Games/Skyrim/Misc/BSScript.h
+++ Code/client/Games/Skyrim/Misc/BSScript.h
@@ -308,6 +308,8 @@ struct BSScript
         bool MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame) override;
     };
 
+    // Bad PoC code
+#if 0
     template <class... T> struct EventArguments : IFunctionArguments
     {
         using Tuple = std::tuple<EventArguments...>;
@@ -331,6 +333,7 @@ struct BSScript
 
         Tuple args;
     };
+#endif
 };
 
 template <> void BSScript::Variable::Set(int32_t aValue) noexcept;
