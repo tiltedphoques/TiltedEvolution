9714d7b447246ebce39a94e8265fedb20f013c56
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-08-07 19:02:30 +0200

    tweak: fix formatting

--- Code/encoding/Messages/ServerTimeSettings.cpp
+++ Code/encoding/Messages/ServerTimeSettings.cpp
@@ -1,10 +1,11 @@
-
 #include <Messages/ServerTimeSettings.h>
 
-void ServerTimeSettings::SerializeRaw(TiltedPhoques::Buffer::Writer& aWriter) const noexcept { timeModel.Serialize(aWriter); }
+void ServerTimeSettings::SerializeRaw(TiltedPhoques::Buffer::Writer& aWriter) const noexcept
+{
+    timeModel.Serialize(aWriter);
+}
 
-void ServerTimeSettings::DeserializeRaw(TiltedPhoques::Buffer::Reader& aReader) noexcept { timeModel.Deserialize(aReader); }
-
-bool ServerTimeSettings::operator==(const ServerTimeSettings& achRhs) const noexcept { return GetOpcode() == achRhs.GetOpcode() && timeModel == achRhs.timeModel; }
-
-bool ServerTimeSettings::operator!=(const ServerTimeSettings& achRhs) const noexcept { return !this->operator==(achRhs); }
+void ServerTimeSettings::DeserializeRaw(TiltedPhoques::Buffer::Reader& aReader) noexcept
+{
+    timeModel.Deserialize(aReader);
+}
--- Code/encoding/Messages/ServerTimeSettings.h
+++ Code/encoding/Messages/ServerTimeSettings.h
@@ -15,8 +15,15 @@ struct ServerTimeSettings final : ServerMessage
     void SerializeRaw(TiltedPhoques::Buffer::Writer& aWriter) const noexcept override;
     void DeserializeRaw(TiltedPhoques::Buffer::Reader& aReader) noexcept override;
 
-    bool operator==(const ServerTimeSettings& achRhs) const noexcept;
-    bool operator!=(const ServerTimeSettings& achRhs) const noexcept;
+    bool operator==(const ServerTimeSettings& achRhs) const noexcept
+    {
+        return GetOpcode() == achRhs.GetOpcode() && timeModel == achRhs.timeModel;
+    }
+
+    bool operator!=(const ServerTimeSettings& achRhs) const noexcept
+    {
+        return !this->operator==(achRhs);
+    }
 
     TimeModel timeModel;
 };
