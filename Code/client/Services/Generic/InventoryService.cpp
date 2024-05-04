e27bb3730d7f8fb87c3303553f3f119330b96fc7
Robbe Bryssinck <robbebryssinck@gmail.com>
2024-05-04 22:35:50 +0200

    fix: dont send queued (un)equip messages

--- Code/client/Services/Generic/InventoryService.cpp
+++ Code/client/Services/Generic/InventoryService.cpp
@@ -173,19 +173,9 @@ void InventoryService::OnNotifyEquipmentChanges(const NotifyEquipmentChanges& ac
     uint32_t equipSlotId = modSystem.GetGameId(acMessage.EquipSlotId);
     TESForm* pEquipSlot = TESForm::GetById(equipSlotId);
 
-    // TODO: ft, does it have the same problem?
-#if TP_SKYRIM64
     uint32_t slotId = 0;
     if (pEquipSlot == DefaultObjectManager::Get().rightEquipSlot)
         slotId = 1;
-
-    // There's a bug where double equipping something magically unequips something secretly.
-    // TODO: should this be done for armor as well?
-    // Also, find out why the client is sending two equip messages in the first place.
-    // TODO: this fix makes it so that weapons and spells are sometimes invisible :/
-    if (!acMessage.Unequip && pActor->GetEquippedWeapon(slotId) == pItem)
-        return;
-#endif
 
     auto* pEquipManager = EquipManager::Get();
 
