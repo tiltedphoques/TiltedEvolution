f0506d809b2cb57dfdd57accbebca91c05c62449
Daniil Zakharov <miredirex@gmail.com>
2024-10-31 00:50:22 +0300

    Change `D3D11CreateDevice` invocation in NvidiaUtil to a dynamic one
    Black screen fix still applies. Dynamic invocation is needed in order not to link `d3d11.dll` at compile-time (compile-time linking breaks ENB and other mods that use d3d11.dll as a proxy)
    
    Co-authored-by: rfortier <rwf@ourchateau.net>

--- Code/client/NvidiaUtil.cpp
+++ Code/client/NvidiaUtil.cpp
@@ -7,10 +7,19 @@ bool IsNvidiaOverlayLoaded()
 }
 
 // This makes the Nvidia overlay happy.
-// The call to D3D11CreateDevice probably causes some of their
+// The call to `D3D11CreateDevice` probably causes some of their
 // internal hooks to be called and do the required init work before the game window opens.
-HRESULT CreateEarlyDxDevice(ID3D11Device* apOutDevice, D3D_FEATURE_LEVEL* apOutFeatureLevel)
+//
+// We do dynamic invocation in order not to link `d3d11.dll` at compile-time
+HRESULT CreateEarlyDxDevice(ID3D11Device** appOutDevice, D3D_FEATURE_LEVEL* apOutFeatureLevel)
 {
-    return D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &apOutDevice,
-                             apOutFeatureLevel, nullptr);
+    HMODULE d3d11Module = GetModuleHandleW(L"d3d11.dll");
+    if (!d3d11Module)
+        return E_FAIL;
+
+    const auto pD3D11CreateDeviceFn =
+        reinterpret_cast<decltype(&D3D11CreateDevice)>(GetProcAddress(d3d11Module, "D3D11CreateDevice"));
+
+    return pD3D11CreateDeviceFn(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION,
+                                appOutDevice, apOutFeatureLevel, nullptr);
 }
