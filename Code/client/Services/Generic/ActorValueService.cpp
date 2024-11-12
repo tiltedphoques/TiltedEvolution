cff5144a480cfd786324e90952b3a3aa2d4314be
otsffs <141402668+otsffs@users.noreply.github.com>
2024-11-12 15:37:25 +0800

    fix requestMaxValueChanges (#734)

--- Code/client/Services/Generic/ActorValueService.cpp
+++ Code/client/Services/Generic/ActorValueService.cpp
@@ -138,7 +138,7 @@ void ActorValueService::BroadcastActorValues() noexcept
             float oldMaxValue = actorValuesComponent.CurrentActorValues.ActorMaxValuesList[i];
             if (newMaxValue != oldMaxValue)
             {
-                requestValueChanges.Values.insert({i, newValue});
+                requestMaxValueChanges.Values.insert({i, newMaxValue});
                 actorValuesComponent.CurrentActorValues.ActorMaxValuesList[i] = newMaxValue;
             }
         }
