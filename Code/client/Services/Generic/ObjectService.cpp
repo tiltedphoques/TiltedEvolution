e0978bf071cbcba54b3d2cd8be16fd737e5f02d4
Daniil <miredirex@gmail.com>
2025-02-11 10:54:06 +0300

    Exclude the equipment chest from sync in "No One Escapes Cidhna Mine" (#765)
    Otherwise the party leader overrides everyone's stuff

--- Code/client/Services/Generic/ObjectService.cpp
+++ Code/client/Services/Generic/ObjectService.cpp
@@ -67,9 +67,12 @@ bool ShouldSyncObject(const TESObjectREFR* apObject) noexcept
 
     switch (apObject->formID)
     {
-        // Don't sync the chest in the "Diplomatic Immunity" quest
-    case 0x39CF1: return false;
-    default: return true;
+    case 0x39CF1: // Don't sync the chest in the "Diplomatic Immunity" quest
+        return false;
+    case 0x3EF03: // ...as well as in the "No One Escapes Cidhna Mine" quest
+        return false;
+    default:
+        return true;
     }
 }
 
