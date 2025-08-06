rfortier/TiltedEvolution-rwf

Add in-game message box that warns about launching from the wrong exe

Code/client/Games/Misc/BSScript.cpp
@@ -34,8 +34,8 @@ void TP_MAKE_THISCALL(HookRegisterPapyrusFunction, BSScript::IVirtualMachine, Na
 void TP_MAKE_THISCALL(HookBindEverythingToScript, BSScript::IVirtualMachine*)
 {
     (*apThis)->BindNativeMethod(new BSScript::IsRemotePlayerFunc("IsRemotePlayer", "SkyrimTogetherUtils", PapyrusFunctions::IsRemotePlayer, BSScript::Variable::kBoolean));
-
     (*apThis)->BindNativeMethod(new BSScript::IsPlayerFunc("IsPlayer", "SkyrimTogetherUtils", PapyrusFunctions::IsPlayer, BSScript::Variable::kBoolean));
+    (*apThis)->BindNativeMethod(new BSScript::IsSkyrimTogetherExeRunningFunc("IsSkyrimTogetherExeRunning", "SkyrimTogetherVerifyLaunchScript", PapyrusFunctions::IsSkyrimTogetherExeRunning, BSScript::Variable::kBoolean));
 
     TiltedPhoques::ThisCall(RealBindEverythingToScript, apThis);
 }

Code/client/Games/Skyrim/Misc/BSScript.cpp
@@ -289,3 +289,16 @@ bool BSScript::IsPlayerFunc::MarshallAndDispatch(Variable* apBaseVar, IVirtualMa
 
     return true;
 }
+
+BSScript::IsSkyrimTogetherExeRunningFunc::IsSkyrimTogetherExeRunningFunc(const char* apFunctionName, const char* apClassName, FunctionType aFunction, Variable::Type aType)
+    : NativeFunction(apFunctionName, apClassName, true, 0)
+{
+    pFunction = reinterpret_cast<void*>(aFunction);
+    returnType = aType;
+}
+
+bool BSScript::IsSkyrimTogetherExeRunningFunc::MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame)
+{
+    apResult->Set<bool>(true);
+    return true;
+}

Code/client/Games/Skyrim/Misc/BSScript.h
@@ -308,6 +308,15 @@ struct BSScript
         bool MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame) override;
     };
 
+    struct IsSkyrimTogetherExeRunningFunc : NativeFunction
+    {
+        using FunctionType = bool();
+
+        IsSkyrimTogetherExeRunningFunc(const char* apFunctionName, const char* apClassName, FunctionType aFunction, Variable::Type aType);
+
+        bool MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame) override;
+    };
+
     // Bad PoC code
 #if 0
     template <class... T> struct EventArguments : IFunctionArguments

tiltedphoques/TiltedEvolution

Code/client/Games/Misc/BSScript.cpp
@@ -34,8 +34,8 @@ void TP_MAKE_THISCALL(HookRegisterPapyrusFunction, BSScript::IVirtualMachine, Na
 void TP_MAKE_THISCALL(HookBindEverythingToScript, BSScript::IVirtualMachine*)
 {
     (*apThis)->BindNativeMethod(new BSScript::IsRemotePlayerFunc("IsRemotePlayer", "SkyrimTogetherUtils", PapyrusFunctions::IsRemotePlayer, BSScript::Variable::kBoolean));
-
     (*apThis)->BindNativeMethod(new BSScript::IsPlayerFunc("IsPlayer", "SkyrimTogetherUtils", PapyrusFunctions::IsPlayer, BSScript::Variable::kBoolean));
+    (*apThis)->BindNativeMethod(new BSScript::DidLaunchSkyrimTogetherFunc("DidLaunchSkyrimTogether", "SkyrimTogetherVerifyLaunchScript", PapyrusFunctions::DidLaunchSkyrimTogether, BSScript::Variable::kBoolean));
 
     TiltedPhoques::ThisCall(RealBindEverythingToScript, apThis);
 }

Code/client/Games/Skyrim/Misc/BSScript.cpp
@@ -289,3 +289,16 @@ bool BSScript::IsPlayerFunc::MarshallAndDispatch(Variable* apBaseVar, IVirtualMa
 
     return true;
 }
+
+BSScript::DidLaunchSkyrimTogetherFunc::DidLaunchSkyrimTogetherFunc(const char* apFunctionName, const char* apClassName, FunctionType aFunction, Variable::Type aType)
+    : NativeFunction(apFunctionName, apClassName, true, 0)
+{
+    pFunction = reinterpret_cast<void*>(aFunction);
+    returnType = aType;
+}
+
+bool BSScript::DidLaunchSkyrimTogetherFunc::MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame)
+{
+    apResult->Set<bool>(true);
+    return true;
+}

Code/client/Games/Skyrim/Misc/BSScript.h
@@ -308,6 +308,15 @@ struct BSScript
         bool MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame) override;
     };
 
+    struct DidLaunchSkyrimTogetherFunc : NativeFunction
+    {
+        using FunctionType = bool();
+
+        DidLaunchSkyrimTogetherFunc(const char* apFunctionName, const char* apClassName, FunctionType aFunction, Variable::Type aType);
+
+        bool MarshallAndDispatch(Variable* apBaseVar, IVirtualMachine* apVm, uint32_t aStackID, Variable* apResult, StackFrame* apStackFrame) override;
+    };
+
     // Bad PoC code
 #if 0
     template <class... T> struct EventArguments : IFunctionArguments