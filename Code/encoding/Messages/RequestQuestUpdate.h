821205420c548b94f65900726a36aeb76636703a
Richard Fortier <rfortier@ourchateau.net>
2025-10-14 17:14:30 -0400

    Separate commit for this longstanding bugfix so it can be cherry-picked into its own PR

--- Code/encoding/Messages/RequestQuestUpdate.h
+++ Code/encoding/Messages/RequestQuestUpdate.h
@@ -15,7 +15,7 @@ struct RequestQuestUpdate final : ClientMessage
     void SerializeRaw(TiltedPhoques::Buffer::Writer& aWriter) const noexcept override;
     void DeserializeRaw(TiltedPhoques::Buffer::Reader& aReader) noexcept override;
 
-    bool operator==(const RequestQuestUpdate& acRhs) const noexcept { return GetOpcode() == acRhs.GetOpcode() && Id == acRhs.Id && Stage == acRhs.Stage && Status == acRhs.Stage && ClientQuestType == acRhs.ClientQuestType; }
+    bool operator==(const RequestQuestUpdate& acRhs) const noexcept { return GetOpcode() == acRhs.GetOpcode() && Id == acRhs.Id && Stage == acRhs.Stage && Status == acRhs.Status && ClientQuestType == acRhs.ClientQuestType; }
 
     enum StatusCode : uint8_t
     {
