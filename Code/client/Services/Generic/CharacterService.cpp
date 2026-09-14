865449a48080095f9ba90e0ef2a0a58aed5357eb
Daniil Zakharov <miredirex@gmail.com>
2026-09-14 16:50:01 +0300

    Fall back to `BaseId` if `LeveledNpcPickId` produces a null TESNPC
    This can happen when players don't have the exact same load order

--- Code/client/Services/Generic/CharacterService.cpp
+++ Code/client/Services/Generic/CharacterService.cpp
@@ -499,24 +499,18 @@ void CharacterService::OnCharacterSpawn(const CharacterSpawnRequest& acMessage) 
         if (acMessage.BaseId != GameId{})
         {
             // Prefer the owner's resolved leveled pick over the lossy template base
-            GameId baseId = acMessage.BaseId;
-            uint32_t npcId = World::Get().GetModSystem().GetGameId(baseId);
             if (acMessage.LeveledNpcPickId != GameId{})
-            {
-                if (const uint32_t cPickNpcId = World::Get().GetModSystem().GetGameId(acMessage.LeveledNpcPickId))
-                {
-                    baseId = acMessage.LeveledNpcPickId;
-                    npcId = cPickNpcId;
-                }
-            }
+                pNpc = Cast<TESNPC>(TESForm::GetById(m_world.GetModSystem().GetGameId(acMessage.LeveledNpcPickId)));
 
-            if (npcId == 0)
+            if (!pNpc)
+                pNpc = Cast<TESNPC>(TESForm::GetById(m_world.GetModSystem().GetGameId(acMessage.BaseId)));
+
+            if (!pNpc)
             {
-                spdlog::error("Failed to retrieve NPC, it will not be spawned, possibly missing mod, base: {:X}:{:X}, form: {:X}:{:X}", baseId.BaseId, baseId.ModId, acMessage.FormId.BaseId, acMessage.FormId.ModId);
+                spdlog::error("Failed to retrieve NPC, it will not be spawned, possibly missing mod, base: {:X}:{:X}, form: {:X}:{:X}", acMessage.BaseId.BaseId, acMessage.BaseId.ModId, acMessage.FormId.BaseId, acMessage.FormId.ModId);
                 return;
             }
 
-            pNpc = Cast<TESNPC>(TESForm::GetById(npcId));
             pNpc->Deserialize(acMessage.AppearanceBuffer, acMessage.ChangeFlags);
         }
         else
@@ -1512,24 +1506,18 @@ Actor* CharacterService::CreateCharacterForEntity(entt::entity aEntity) const no
         if (acMessage.BaseId != GameId{})
         {
             // Prefer the owner's resolved leveled pick over the lossy template base
-            GameId baseId = acMessage.BaseId;
-            uint32_t npcId = World::Get().GetModSystem().GetGameId(baseId);
             if (acMessage.LeveledNpcPickId != GameId{})
-            {
-                if (const uint32_t cPickNpcId = World::Get().GetModSystem().GetGameId(acMessage.LeveledNpcPickId))
-                {
-                    baseId = acMessage.LeveledNpcPickId;
-                    npcId = cPickNpcId;
-                }
-            }
+                pNpc = Cast<TESNPC>(TESForm::GetById(m_world.GetModSystem().GetGameId(acMessage.LeveledNpcPickId)));
 
-            if (npcId == 0)
+            if (!pNpc)
+                pNpc = Cast<TESNPC>(TESForm::GetById(m_world.GetModSystem().GetGameId(acMessage.BaseId)));
+
+            if (!pNpc)
             {
                 spdlog::error("Failed to retrieve NPC, it will not be spawned, possibly missing mod");
                 return nullptr;
             }
 
-            pNpc = Cast<TESNPC>(TESForm::GetById(npcId));
             pNpc->Deserialize(acMessage.AppearanceBuffer, acMessage.ChangeFlags);
         }
         else
